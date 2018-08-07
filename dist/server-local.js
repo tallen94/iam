425966e38f4684275d4d6a17298bf3e717a32b00
Trevor Allen <tallen94@uw.edu>
2018-08-07 00:26:38 -0700

    Removes all the files

--- dist/server-local.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const modules_1 = require("./modules/modules");
-const cluster = new modules_1.Cluster(5);
-cluster.startCluster().then(() => {
-    console.log("Started");
-});
-//# sourceMappingURL=server-local.js.map
