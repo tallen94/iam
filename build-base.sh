722d1bee57d7b50dfd4200b26597aa94c8177312
Trevor Allen <tallen94@uw.edu>
2019-11-23 00:57:29 -0800

    Adding builds for other apps

--- build-base.sh
+++ build-base.sh
@@ -1,4 +1,247 @@
 #!/bin/bash
+
+TAG="icanplayguitar94/iam:base-$1"
+
 npm install --prefix src && npm run --prefix src build && npm run --prefix src pkg && mv src/deploy-1.0.0.tgz images/base/deploy.tgz
 
-cd images && ./bake.sh base $GITHUB_SHA
+docker build --no-cache -t $TAG images/base
+
+cat > kubernetes/apps/executor.yaml <<EOF
+apiVersion: apps/v1 # for versions before 1.9.0 use apps/v1beta2
+kind: Deployment
+metadata:
+  name: iam-executor
+spec:
+  selector:
+    matchLabels:
+      app: iam-executor
+  replicas: 1 # tells deployment to run 2 pods matching the template
+  template:
+    metadata:
+      labels:
+        app: iam-executor
+    spec:
+      imagePullSecrets:
+      - name: regcred
+      containers:
+      - name: iam-executor
+        image: $TAG
+        imagePullPolicy: IfNotPresent
+        ports:
+        - containerPort: 5000
+        readinessProbe:
+          httpGet:
+            path: /status
+            port: 5000
+          initialDelaySeconds: 3
+          periodSeconds: 3
+        env:
+        - name: HOME
+          value: "/usr/home/iam"
+        - name: TYPE 
+          value: "executor"
+        - name: SERVER_PORT
+          value: "5000"
+
+        # FS CONFIG
+        - name: FS_HOST
+          value: "iam-filesystem"
+        - name: FS_PORT
+          value: "80"
+
+        ## DB CONFIG
+        - name: DB_USER
+          valueFrom:
+            secretKeyRef:
+              name: dbconfig
+              key: user
+        - name: DB_PASSWORD
+          valueFrom:
+            secretKeyRef:
+              name: dbconfig
+              key: password
+        - name: DB_HOST
+          value: "mysqldatabase.default"
+        - name: DB_NAME
+          valueFrom:
+            secretKeyRef:
+              name: dbconfig
+              key: db_name
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: iam-executor
+spec:
+  selector:
+    app: iam-executor
+  ports:
+    - protocol: TCP
+      port: 80
+      targetPort: 5000
+EOF
+
+cat > kubernetes/apps/master.yaml <<EOF
+apiVersion: apps/v1 # for versions before 1.9.0 use apps/v1beta2
+kind: Deployment
+metadata:
+  name: iam-master
+spec:
+  selector:
+    matchLabels:
+      app: iam-master
+  replicas: 1 # tells deployment to run 2 pods matching the template
+  template:
+    metadata:
+      labels:
+        app: iam-master
+    spec:
+      imagePullSecrets:
+      - name: regcred
+      containers:
+      - name: iam-master
+        image: $TAG
+        imagePullPolicy: IfNotPresent
+        ports:
+        - containerPort: 5000
+        readinessProbe:
+          httpGet:
+            path: /status
+            port: 5000
+          initialDelaySeconds: 3
+          periodSeconds: 3
+        env:
+        - name: HOME
+          value: "/usr/home/iam"
+        - name: TYPE 
+          value: "master"
+        - name: SERVER_PORT
+          value: "5000"
+
+        # FS CONFIG
+        - name: FS_HOST
+          value: "iam-filesystem"
+        - name: FS_PORT
+          value: "80"
+
+        ## DB CONFIG
+        - name: DB_USER
+          valueFrom:
+            secretKeyRef:
+              name: dbconfig
+              key: user
+        - name: DB_PASSWORD
+          valueFrom:
+            secretKeyRef:
+              name: dbconfig
+              key: password
+        - name: DB_HOST
+          value: "mysqldatabase.default"
+        - name: DB_NAME
+          valueFrom:
+            secretKeyRef:
+              name: dbconfig
+              key: db_name
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: iam-master
+spec:
+  selector:
+    app: iam-master
+  type: NodePort
+  ports:
+    - protocol: TCP
+      port: 80
+      targetPort: 5000
+      nodePort: 30001
+EOF
+
+cat > kubernetes/apps/job.yaml <<EOF
+apiVersion: apps/v1 # for versions before 1.9.0 use apps/v1beta2
+kind: Deployment
+metadata:
+  name: iam-job
+spec:
+  selector:
+    matchLabels:
+      app: iam-job
+  replicas: 1 # tells deployment to run 2 pods matching the template
+  template:
+    metadata:
+      labels:
+        app: iam-job
+    spec:
+      imagePullSecrets:
+      - name: regcred
+      containers:
+      - name: iam-job
+        image: $TAG
+        imagePullPolicy: IfNotPresent
+        ports:
+        - containerPort: 5000
+        readinessProbe:
+          httpGet:
+            path: /status
+            port: 5000
+          initialDelaySeconds: 3
+          periodSeconds: 3
+        env:
+        - name: HOME
+          value: "/usr/home/iam"
+        - name: TYPE 
+          value: "job"
+        - name: SERVER_PORT
+          value: "5000"
+
+        # FS CONFIG
+        - name: FS_HOST
+          value: "iam-filesystem"
+        - name: FS_PORT
+          value: "80"
+
+        ## DB CONFIG
+        - name: DB_USER
+          valueFrom:
+            secretKeyRef:
+              name: dbconfig
+              key: user
+        - name: DB_PASSWORD
+          valueFrom:
+            secretKeyRef:
+              name: dbconfig
+              key: password
+        - name: DB_HOST
+          value: "mysqldatabase.default"
+        - name: DB_NAME
+          valueFrom:
+            secretKeyRef:
+              name: dbconfig
+              key: db_name
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: iam-job
+spec:
+  selector:
+    app: iam-job
+  ports:
+    - protocol: TCP
+      port: 80
+      targetPort: 5000
+EOF
+
+
+cat > images/dashboard/Dockerfile <<EOF
+FROM $TAG
+COPY ./src public/dist
+EOF
+./build-dashboard $1
+
+cat > images/filesystem/Dockerfile <<EOF
+FROM $TAG
+ADD programs /usr/home/iam/programs
+EOF
+./build-filesystem $1
--- build-dependencies.sh
+++ build-dependencies.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-cd images && ./bake.sh dependencies $1
+TAG="icanplayguitar94/iam:dependencies-$1"
+
+docker build --no-cache -t $TAG images/dependencies
+
+cat > kubernetes/base/Dockerfile <<EOF
+FROM $TAG
+
+WORKDIR /usr/home/iam
+
+COPY deploy.tgz deploy.tgz
+RUN npm i -g deploy.tgz
+
+EXPOSE 5000
+
+CMD deploy
+EOF
+./build-base $1
