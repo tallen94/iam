3d0be470088a732382b2f065fd9d0393e3d796d7
trevor <tallen94@uw.edu>
2019-12-07 20:12:47 -0800

    Exporting new schema data

--- builders/build-database.sh
+++ builders/build-database.sh
@@ -4,6 +4,17 @@
 TAG="icanplayguitar94/iam:database-$1"
 PUSH=$2
 
+mysqldump -u iam -p -h iam-local -P 30002 --databases iam > images/database/database.sql
+
+cat >> images/database/database.sql <<EOF
+-- Create Users
+CREATE USER '\$MYSQL_USER'@'%' IDENTIFIED BY '\$MYSQL_PASSWORD';
+GRANT SELECT ON \`\$MYSQL_DATABASE\`.* TO '\$MYSQL_USER'@'%';
+GRANT UPDATE ON \`\$MYSQL_DATABASE\`.* TO '\$MYSQL_USER'@'%';
+GRANT DELETE ON \`\$MYSQL_DATABASE\`.* TO '\$MYSQL_USER'@'%';
+GRANT INSERT ON \`\$MYSQL_DATABASE\`.* TO '\$MYSQL_USER'@'%';
+EOF
+
 # Build docker container and push
 docker build --no-cache -t $TAG images/database
 
