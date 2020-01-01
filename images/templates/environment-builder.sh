5282fdb67945e72e51145f6457b98fdc86df3260
trevor <tallen94@uw.edu>
2020-01-01 12:27:18 -0800

    Adding build pipeline for environment-builder

--- images/templates/environment-builder.sh
+++ images/templates/environment-builder.sh
@@ -1,4 +1,8 @@
-FROM icanplayguitar94/iam:base-21d4a90e0272a4173c43a55a4bdfa598bc11a009
+#!/bin/bash
+TAG=$1
+
+cat > images/environment-builder/Dockerfile <<EOF
+FROM $TAG
 
 RUN apt-get update
 RUN apt-get install docker.io -y
@@ -6,3 +10,4 @@ RUN apt-get install docker.io -y
 RUN curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 RUN chmod +x ./kubectl
 RUN mv ./kubectl /usr/local/bin/kubectl
+EOF
